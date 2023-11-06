3427c31ac06390934e74e575ab1df95515ef15f9
LaisLatifi <mudaser1992@gmail.com>
2023-11-06 13:23:02 -0800

    2nd commit

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Iam JenkinsNov01");
+        System.out.println("Iam JenkinsNov01 study time starts ");
     }
 }
